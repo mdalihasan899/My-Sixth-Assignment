9dccca347a6415ca2e8254500dfb90784fcc31f9
mdalihasan899 <mdalihn899@gmail.com>
2025-09-09 15:22:18 +0600

    categories button activation done

--- script/index.js
+++ script/index.js
@@ -9,17 +9,28 @@ const loadPlants = (id) => {
   const url = `https://openapi.programming-hero.com/api/category/${id}`;
   fetch(`https://openapi.programming-hero.com/api/category/${id}`)
     .then((res) => res.json())
-    .then((json) => displayDifPlants(json.plants))
+    .then((data) => {
+      removeActive();
+      const categorieButton = document.getElementById(`category-btn-${id}`);
+      categorieButton.classList.add("active");
+      displayDifPlants(data.plants)
+    })
+}
+
+const removeActive = () => {
+  const newCategorieBtns = document.querySelectorAll(".category-btn");
+  newCategorieBtns.forEach((newCategorieBtn)=>newCategorieBtn.classList.remove("active")); 
+
 }
 
 const displayDifPlants = (DifPlants) => {
-  const plantsContainer=document.getElementById("card-container");
-  plantsContainer.innerHTML="";
-  DifPlants.forEach(DifPlant => { 
-    const plantsByCategoris=document.createElement("div");
-    plantsByCategoris.innerHTML=`
+  const plantsContainer = document.getElementById("card-container");
+  plantsContainer.innerHTML = "";
+  DifPlants.forEach(DifPlant => {
+    const plantsByCategoris = document.createElement("div");
+    plantsByCategoris.innerHTML = `
     <div class="max-w-sm bg-white rounded-lg shadow p-4 items-center">
-            <img class="w-full h-40 rounded-md mb-4" src="${DifPlant.image}" alt="">
+            <img class="w-full h-40 object-cover rounded-md mb-4" src="${DifPlant.image}" alt="">
             <h3 class="font-semibold text-gray-800">${DifPlant.name}</h3>
             <p class="text-sm text-gray-600 mt-1 line-clamp-3">
               ${DifPlant.description}
@@ -49,9 +60,9 @@ const displayCategories = (categories) => {
   for (const categorie of categories) {
     const categorieBtn = document.createElement("div");
     categorieBtn.innerHTML = `
-        <button onclick="loadPlants(${categorie.id})" href=""
-            class="hover:bg-[#15803D] hover:scale-105 hover:text-white w-full text-start py-2 pl-[10px] rounded-[4px] mt-1">${categorie.category_name}</button>
-        `
+        <button id="category-btn-${categorie.id}" onclick="loadPlants(${categorie.id})" href=""
+            class="hover:bg-[#15803D] hover:scale-105 hover:text-white w-full text-start py-2 pl-[10px] rounded-[4px] mt-1 category-btn">${categorie.category_name}</button>
+        `   
     categoriesConteiner.appendChild(categorieBtn);
   }
 
@@ -73,7 +84,7 @@ const displayAllTrees = (Trees) => {
     const TreeCard = document.createElement("div");
     TreeCard.innerHTML = `
         <div class="max-w-sm bg-white rounded-lg shadow p-4 h-full items-center">
-            <img class="w-full h-40 rounded-md mb-4" src="${Tree.image}" alt="">
+            <img class="w-full h-40 object-cover rounded-md mb-4" src="${Tree.image}" alt="">
             <h3 class="font-semibold text-gray-800">${Tree.name}</h3>
             <p class="text-sm text-gray-600 mt-1 line-clamp-3">
               ${Tree.description}
